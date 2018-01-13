7047f83627f9e7bdaa0ce8d82b124653174590c2
Aleksandr Tishchenko <at@MBP-Aleksandr.Dlink>
2018-01-14 01:43:06 +0200

    socket

--- src/server/sockets.ts
+++ src/server/sockets.ts
@@ -3,8 +3,10 @@ import { IO } from './index';
 export function userSocketsEmit(user: string, event: string, payload: any) {
   Object.keys(IO.sockets.connected).map(k => {
     const socket = IO.sockets.connected[k];
-    console.log('socket.handshake', socket.handshake);
-    if (socket.handshake.query.user === user) { socket.emit(event, payload); }
-    // socket.emit(event, payload);
+    console.log('socket.handshake.query.user', socket.handshake.query.user);
+    if (socket.handshake.query.user === user) { socket.emit(event, payload); } else {
+      console.log('NOT FOUND EMIT', socket.handshake);
+      socket.emit(event, payload);
+    }
   });
 }
