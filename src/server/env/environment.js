707c7226851969a0efefb84393440dcfb66557c3
Aleksandr Tishchenko <at@MBP-Aleksandr.Dlink>
2018-02-15 15:59:20 +0200

    fox

--- src/server/env/environment.js
+++ src/server/env/environment.js
@@ -29,10 +29,10 @@ exports.sqlConfig = {
     }
 };
 exports.sqlConfigAccounts = {
-    database: process.env.DB_NAME || 'accounts',
+    database: process.env.DB_ACCOUNTS_NAME || 'accounts',
     server: process.env.DB_ACCOUNTS_HOST || 'db.jetti-app.com',
-    user: process.env.DB_USER || 'jetti-app',
-    password: process.env.DB_PASSWORD || 'Pa$$word',
+    user: process.env.DB_ACCOUNTS_USER || 'jetti-app',
+    password: process.env.DB_ACCOUNTS_PASSWORD || 'Pa$$word',
     options: {
         encrypt: true,
     }
