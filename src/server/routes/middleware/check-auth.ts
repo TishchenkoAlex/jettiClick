a852ce3e5a66cbbc6c0dfc988ff3c9835aaa103b
Aleksandr Tishchenko <at@MBP-Aleksandr.Dlink>
2018-03-15 20:30:29 +0200

    fix

--- src/server/routes/middleware/check-auth.ts
+++ src/server/routes/middleware/check-auth.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
+import * as socketIO from 'socket.io';
 
 import { JTW_KEY } from '../../env/environment';
 import { IJWTPayload } from '../auth';
