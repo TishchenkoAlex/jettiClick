40ee58108df9f725321e416bfdf83f13297259f9
Aleksandr Tishchenko <at@MacBook-Pro-Aleksandr.local>
2018-03-21 20:27:15 +0200

    fix

--- src/client/app/common/forms/base.form.component.ts
+++ src/client/app/common/forms/base.form.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit, TemplateRef } from '@angular/core';
+import { CdkTrapFocus } from '@angular/cdk/a11y';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, QueryList, ViewChildren } from '@angular/core';
 import { ObservableMedia } from '@angular/flex-layout';
 import { FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { take } from 'rxjs/operators';
 
 import { AuthService } from '../../auth/auth.service';
 import { DocService } from '../../common/doc.service';
@@ -14,15 +14,14 @@ import { TabsStore } from '../tabcontroller/tabs.store';
   selector: 'j-forms',
   templateUrl: './base.form.component.html'
 })
-export class BaseFormComponent implements OnInit {
-  @Input() formTepmlate: TemplateRef<any>;
-  @Input() actionTepmlate: TemplateRef<any>;
+export class BaseFormComponent {
 
   @Input() id = Math.random().toString();
   @Input() type = this.route.snapshot.params.type as string;
   @Input() form: FormGroup = this.route.snapshot.data.detail;
+  @ViewChildren(CdkTrapFocus) cdkTrapFocus: QueryList<CdkTrapFocus>;
 
-  docDescription = <string>this.form['metadata'].description;
+  get docDescription() { return <string>this.form['metadata'].description; }
   get v() { return <FormControlInfo[]>this.form['orderedControls']; }
   get vk() { return <{ [key: string]: FormControlInfo }>this.form['byKeyControls']; }
   get hasTables() { return !!(<FormControlInfo[]>this.form['orderedControls']).find(t => t.type === 'table'); }
@@ -31,29 +30,33 @@ export class BaseFormComponent implements OnInit {
   constructor(public router: Router, public route: ActivatedRoute, public media: ObservableMedia,
     public cd: ChangeDetectorRef, public ds: DocService, private auth: AuthService, public tabStore: TabsStore) { }
 
-  ngOnInit() {
-  }
-
   private _close() {
     this.tabStore.close(this.tabStore.state.tabs[this.tabStore.selectedIndex]);
+    const returnTab = this.tabStore.state.tabs[this.tabStore.selectedIndex];
+    this.router.navigate([returnTab.docType, returnTab.docID]);
   }
 
   Close() {
     if (this.form.pristine) { this._close(); return; }
     this.ds.confirmationService.confirm({
-      message: 'Discard changes and close?',
-      header: 'Confirmation',
+      header: 'Discard changes and close?',
+      message: '',
       icon: 'fa fa-question-circle',
       accept: this._close.bind(this),
+      reject: this.focus.bind(this),
       key: this.id
     });
     this.cd.detectChanges();
   }
 
+  focus() {
+    this.cdkTrapFocus.find(el => el.autoCapture).focusTrap.focusFirstTabbableElementWhenReady();
+  }
+
   async Execute(): Promise<any> {
     const data = this.form.value;
     return await this.ds.api.jobAdd({
-      job: { id: 'post', description: '(job) post Invoices' },
+      job: { id: 'post', description: '(Post Invoices' },
       type: data.type.id,
       company: data.company.id,
       StartDate: data.StartDate,
