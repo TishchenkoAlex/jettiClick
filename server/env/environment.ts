f44177c5195dcf9f6ebc481ed72795aa7d25874a
Aleksandr Tishchenko <at@MacBook-Pro-Aleksandr.local>
2018-05-05 06:34:14 +0300

    fix

--- server/env/environment.ts
+++ server/env/environment.ts
@@ -4,9 +4,13 @@ export const DB_NAME = process.env.DB_NAME || 'jetti-app';
 export const REDIS_DB_HOST = process.env.REDIS_DB_HOST || '127.0.0.1';
 export const JTW_KEY = process.env.JTW_KEY || 'Pa5315word';
 
+const DB_PORT = isNaN(parseInt(process.env.DB_PORT as string, undefined)) ?
+   14330 : parseInt(process.env.DB_PORT as string, undefined);
+
 export const sqlConfig: config = {
   server: process.env.DB_HOST || 'sql.jetti-app.com',
-  port: 14330,
+  // tslint:disable-next-line:radix
+  port: DB_PORT,
   database: DB_NAME,
   user: process.env.DB_USER || 'yuralex',
   password: process.env.DB_PASSWORD || 'MyNew01Password',
@@ -22,7 +26,7 @@ export const sqlConfig: config = {
 
 export const sqlConfigAccounts: config = {
   server: process.env.DB_ACCOUNTS_HOST || 'sql.jetti-app.com',
-  port: 14330,
+  port: DB_PORT,
   database: process.env.DB_ACCOUNTS_NAME || 'accounts',
   user: process.env.DB_ACCOUNTS_USER || 'yuralex',
   password: process.env.DB_ACCOUNTS_PASSWORD || 'MyNew01Password',
