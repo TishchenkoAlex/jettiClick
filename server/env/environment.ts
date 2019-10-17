e1c623ef04a0649f1a2e4bbae37f52cb95da9473
Aleksandr Tishchenko <at@mbp.local>
2019-10-17 18:00:03 +0300

    env

--- server/env/environment.ts
+++ server/env/environment.ts
@@ -1,29 +1,31 @@
+import { config as dotenv } from 'dotenv';
 import { config } from 'mssql';
 
-export const DB_NAME = process.env.DB_NAME || 'sm';
-export const REDIS_DB_HOST = process.env.REDIS_DB_HOST || '127.0.0.1';
-export const JTW_KEY = process.env.JTW_KEY || 'Pa5315word';
+dotenv();
 
-const DB_PORT = isNaN(parseInt(process.env.DB_PORT as string, undefined)) ?
-  14330 : parseInt(process.env.DB_PORT as string, undefined);
+export const DB_NAME = process.env.DB_NAME!;
+export const REDIS_DB_HOST = process.env.REDIS_DB_HOST!;
+export const JTW_KEY = process.env.JTW_KEY!;
+
+const DB_PORT = parseInt(process.env.DB_PORT as string, undefined);
 
 export const sqlConfig: config = {
-  server: process.env.DB_HOST || 'sql.jetti-app.com', // '35.204.31.43'
+  server: process.env.DB_HOST!, // '35.204.31.43'
   port: DB_PORT,
   database: DB_NAME,
-  user: process.env.DB_USER || 'yuralex',
-  password: process.env.DB_PASSWORD || 'MyNew01Password',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   requestTimeout: 1000 * 120,
   pool: { min: 25, max: 10000/* , acquireTimeoutMillis: 0 */ },
   options: { encrypt: false }
 };
 
 export const sqlConfigAccounts: config = {
-  server: process.env.DB_ACCOUNTS_HOST || 'sql.jetti-app.com',
+  server: process.env.DB_ACCOUNTS_HOST!,
   port: DB_PORT,
-  database: process.env.DB_ACCOUNTS_NAME || 'accounts',
-  user: process.env.DB_ACCOUNTS_USER || 'yuralex',
-  password: process.env.DB_ACCOUNTS_PASSWORD || 'MyNew01Password',
+  database: process.env.DB_ACCOUNTS_NAME!,
+  user: process.env.DB_ACCOUNTS_USER,
+  password: process.env.DB_ACCOUNTS_PASSWORD ,
   requestTimeout: 1000 * 120,
   pool: { min: 25, max: 10000/* , acquireTimeoutMillis: 0 */ },
   options: { encrypt: false }
